61fc0650bc9e82c10be582fd3f1c212fde757748
Matheus Oliveira Santos <matheusfilho98.ms@gmail.com>
2024-02-11 19:33:12 -0300

    feat: create detail screen

---
+++ app/src/main/java/com/matheus0liveira/coffeeshop/detail/view/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.matheus0liveira.coffeeshop.detail.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.matheus0liveira.coffeeshop.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
