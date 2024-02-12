c08718ee380c6f18e4502aa2f746a8b60e8e2a5c
Matheus Oliveira Santos <matheusfilho98.ms@gmail.com>
2024-02-12 17:16:02 -0300

    feat: create detail screen

--- app/src/main/java/com/matheus0liveira/coffeeshop/detail/view/DetailActivity.kt
+++ app/src/main/java/com/matheus0liveira/coffeeshop/detail/view/DetailActivity.kt
@@ -2,6 +2,10 @@ package com.matheus0liveira.coffeeshop.detail.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.matheus0liveira.coffeeshop.R
 import com.matheus0liveira.coffeeshop.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -9,9 +13,34 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityDetailBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+        with(binding) {
+            setSupportActionBar(mainToolbar)
+            supportActionBar?.title = ""
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.arrow_left)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            detailMainImg.setImageResource(R.drawable.big_coffee)
+            detailRatingAverageTxt.text = "5.0"
+            detailRatingCountTxt.text = "(234)"
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> finish()
+            R.id.menu_bottom_favorite -> Log.i("SAVED", "${item.itemId}")
+        }
+
+        return super.onOptionsItemSelected(item)
+
+
     }
 }
